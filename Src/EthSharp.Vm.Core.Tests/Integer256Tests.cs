c37ecc1b07414d306200e58174083dab18acfd66
ajlopez <webmaster@ajlopez.com>
2016-02-14 09:21:29 -0300

    Integer256 to bytes first test

---
+++ Src/EthSharp.Vm.Core.Tests/Integer256Tests.cs
@@ -0,0 +1,23 @@
+﻿namespace EthSharp.Vm.Core.Tests
+{
+    using System;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class Integer256Tests
+    {
+        [TestMethod]
+        public void ZeroToBytes()
+        {
+            Integer256 value = new Integer256();
+
+            var result = value.ToBytes();
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(32, result.Length);
+
+            Assert.IsTrue(result.All(b => b == 0x00));
+        }
+    }
+}
---
+++ Src/EthSharp.Vm.Core/Integer256.cs
@@ -0,0 +1,19 @@
+﻿namespace EthSharp.Vm.Core
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class Integer256
+    {
+        private UInt32[] values = new UInt32[8];
+
+        public byte[] ToBytes()
+        {
+            var bytes = new byte[32];
+
+            return bytes;
+        }
+    }
+}
