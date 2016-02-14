bd08ce39331820c7d1cefe4ade7df5f77a9cbba3
ajlopez <webmaster@ajlopez.com>
2016-02-14 09:35:47 -0300

    Adding one to Integer256 and ToBytes

--- Src/EthSharp.Vm.Core.Tests/Integer256Tests.cs
+++ Src/EthSharp.Vm.Core.Tests/Integer256Tests.cs
@@ -19,5 +19,22 @@
 
             Assert.IsTrue(result.All(b => b == 0x00));
         }
+
+        [TestMethod]
+        public void OneToBytes()
+        {
+            Integer256 value = new Integer256();
+
+            var newvalue = value.Add(1);
+            var result = newvalue.ToBytes();
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(32, result.Length);
+
+            for (int k = 0; k < 31; k++)
+                Assert.AreEqual(0x00, result[k]);
+
+            Assert.AreEqual(0x01, result[31]);
+        }
     }
 }
--- Src/EthSharp.Vm.Core/Integer256.cs
+++ Src/EthSharp.Vm.Core/Integer256.cs
@@ -7,11 +7,46 @@
 
     public class Integer256
     {
-        private UInt32[] values = new UInt32[8];
+        private const int size = 8;
+        private UInt32[] values;
+
+        public Integer256()
+        {
+            this.values = new UInt32[size];
+        }
+
+        private Integer256(UInt32[] values)
+        {
+            this.values = values;
+        }
+
+        public Integer256 Add(uint value)
+        {
+            var newvalues = new UInt32[8];
+
+            newvalues[0] = values[0] + value;
+
+            return new Integer256(newvalues);
+        }
 
         public byte[] ToBytes()
         {
             var bytes = new byte[32];
+            int offset = 32;
+
+            for (int k = 0; k < size; k++)
+            {
+                UInt32 value = values[k];
+                offset -= 4;
+
+                bytes[offset + 3] = (byte)(value & 0x00ff);
+                value >>= 8;
+                bytes[offset + 2] = (byte)(value & 0x00ff);
+                value >>= 8;
+                bytes[offset + 1] = (byte)(value & 0x00ff);
+                value >>= 8;
+                bytes[offset] = (byte)(value & 0x00ff);
+            }
 
             return bytes;
         }
